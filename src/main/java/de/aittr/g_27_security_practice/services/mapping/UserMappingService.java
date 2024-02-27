679443853a8c23c844f376d0314ee6fe81a652ad
Natalya Selyunina <seluninanv@mail.ru>
2024-02-27 15:25:15 +0300

    feat : method mapRegistrationDtoToEntity in UserMappingService

--- src/main/java/de/aittr/g_27_security_practice/services/mapping/UserMappingService.java
+++ src/main/java/de/aittr/g_27_security_practice/services/mapping/UserMappingService.java
@@ -2,10 +2,12 @@ package de.aittr.g_27_security_practice.services.mapping;
 
 import de.aittr.g_27_security_practice.domain.User;
 import de.aittr.g_27_security_practice.domain.dto.UserDto;
+import de.aittr.g_27_security_practice.domain.dto.UserRegistrationDto;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMappingService {
     UserDto mapEntityToDto(User user);
     User mapDtoToEntity(UserDto userDto);
+    User mapRegistrationDtoToEntity(UserRegistrationDto registrationDto);
 }
