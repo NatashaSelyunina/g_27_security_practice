35932ee7f2720ad49aa3decac7470a7264b7473e
Natalya Selyunina <seluninanv@mail.ru>
2024-02-27 15:01:36 +0300

    feat : generation constructors, getters, setters, equals, hashCode, toString for UserDto, RoleDto

--- src/main/java/de/aittr/g_27_security_practice/domain/RoleDto.java
+++ src/main/java/de/aittr/g_27_security_practice/domain/RoleDto.java
@@ -1,6 +1,53 @@
 package de.aittr.g_27_security_practice.domain;
 
+import java.util.Objects;
+
 public class RoleDto {
     private int id;
     private String name;
+
+    public RoleDto() {
+    }
+
+    public RoleDto(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleDto roleDto = (RoleDto) o;
+        return id == roleDto.id && Objects.equals(name, roleDto.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "RoleDto{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/de/aittr/g_27_security_practice/domain/UserDto.java
+++ src/main/java/de/aittr/g_27_security_practice/domain/UserDto.java
@@ -1,9 +1,65 @@
 package de.aittr.g_27_security_practice.domain;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class UserDto {
     private int id;
     private String username;
     private Set<Role> roles;
+
+    public UserDto() {
+    }
+
+    public UserDto(int id, String username, Set<Role> roles) {
+        this.id = id;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserDto userDto = (UserDto) o;
+        return id == userDto.id && Objects.equals(username, userDto.username) && Objects.equals(roles, userDto.roles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, roles);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
 }
