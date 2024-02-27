5224be65918ce21e07ec3fb5338b8ec8df982ee6
Natalya Selyunina <seluninanv@mail.ru>
2024-02-27 14:58:56 +0300

    feat : classes UserDto, RoleDto with fields

---
+++ src/main/java/de/aittr/g_27_security_practice/domain/RoleDto.java
@@ -0,0 +1,6 @@
+package de.aittr.g_27_security_practice.domain;
+
+public class RoleDto {
+    private int id;
+    private String name;
+}
---
+++ src/main/java/de/aittr/g_27_security_practice/domain/UserDto.java
@@ -0,0 +1,9 @@
+package de.aittr.g_27_security_practice.domain;
+
+import java.util.Set;
+
+public class UserDto {
+    private int id;
+    private String username;
+    private Set<Role> roles;
+}
