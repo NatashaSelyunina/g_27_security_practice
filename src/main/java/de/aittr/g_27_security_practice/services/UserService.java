968dbf424a6b0a5d1b3bb9cefc66cc780a4a4953
Natalya Selyunina <seluninanv@mail.ru>
2024-02-28 01:31:07 +0300

    feat : method registration(), field passwordEncoder in UserService, add constructor with name in class Role

--- src/main/java/de/aittr/g_27_security_practice/services/UserService.java
+++ src/main/java/de/aittr/g_27_security_practice/services/UserService.java
@@ -1,18 +1,32 @@
 package de.aittr.g_27_security_practice.services;
 
+import de.aittr.g_27_security_practice.domain.Role;
 import de.aittr.g_27_security_practice.domain.User;
+import de.aittr.g_27_security_practice.domain.dto.RoleDto;
+import de.aittr.g_27_security_practice.domain.dto.UserDto;
+import de.aittr.g_27_security_practice.domain.dto.UserRegistrationDto;
 import de.aittr.g_27_security_practice.repositories.UserRepository;
+import de.aittr.g_27_security_practice.services.mapping.UserMappingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService implements UserDetailsService {
     private UserRepository repository;
+    private UserMappingService service;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, UserMappingService service) {
         this.repository = repository;
+        this.service = service;
     }
 
     @Override
@@ -24,5 +38,14 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    public UserDto registration(UserRegistrationDto registrationDto) {
+        User entity = service.mapRegistrationDtoToEntity(registrationDto);
+        entity.setId(0);
+        entity.getRoles().add(new Role("USER"));
+        entity.setPassword(passwordEncoder.encode(registrationDto.getPassword()));
+        entity = repository.save(entity);
+        return service.mapEntityToDto(entity);
     }
 }
